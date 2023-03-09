0e17bf0b8f55e96d63097aaae87fa1e54fae0289
unknown <cameronkennedy795@gmail.com>
2023-03-09 12:24:04 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,82 @@
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+  renderOption(movie){
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+    <img src="${imgSrc}">
+     ${movie.Title} (${movie.Year}) (${movie.Type})
+    `;
+  },
+  onOptionSelect(movie){
+    onMovieSelect(movie);
+  },
+  inputValue(movie){
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "276cbec5",
+        s: searchTerm
+      }
+    });
+  
+    if (response.data.Error) {
+      return []
+    }
+  
+    return response.data.Search;
+  },
+});
+
+const onMovieSelect = async movie => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "276cbec5",
+      i: movie.imdbID
+    }
+  });
+ 
+  document.querySelector(".summary").innerHTML = movieTemplate(response.data)
+};
+
+
+
+const movieTemplate = movieDetail => {
+  return `
+    <article class="media">
+    <figure class="media-left"></figure>
+    <p class="image"> </p>
+      <img src="${movieDetail.Poster}">
+      <div class="media-content">
+      <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+      </article>
+      <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+      </article>
+      <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+      </article>
+      <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascorer</p>
+      </article>
+      <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+      </article>
+      <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+      </article>
+  `;
+  
+};
+
